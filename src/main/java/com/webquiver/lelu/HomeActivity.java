978a5b06668e975c36001d337075ec6ea6806d41
Jabin Jawad <jabin@webquiver.com>
2017-07-10 16:50:53 +0530

    10/7/2017  5:35

--- src/main/java/com/webquiver/lelu/HomeActivity.java
+++ src/main/java/com/webquiver/lelu/HomeActivity.java
@@ -1,6 +1,8 @@
 package com.webquiver.lelu;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,9 +13,47 @@ import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+import com.webquiver.lelu.classes.ExpandableHeightGridView;
+import com.webquiver.lelu.classes.GridViewAdapter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class HomeActivity extends AppCompatActivity {
+
+    public static final String DATA_URL = "https://api.myjson.com/bins/xyser";
+
+
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+    public static final String TAG_COLOR = "color";
+
+    //GridView Object
+    private ExpandableHeightGridView gridView;
+
+
+    private ArrayList<String> images;
+    private ArrayList<String> names;
+    private ArrayList<String> color;
+
+
+    CollapsingToolbarLayout collapsingToolbarLayout;
+    LinearLayout categorylayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,13 +63,16 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
          ImageView search = (ImageView) findViewById(R.id.search);
+
+        collapsingToolbarLayout=(CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+        categorylayout=(LinearLayout)findViewById(R.id.categorylyt_id);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-                CollapsingToolbarLayout collapsingToolbarLayout=(CollapsingToolbarLayout)findViewById(R.id.toolbar_layout);
+
 
                 collapsingToolbarLayout.setBackgroundResource(R.drawable.banner2);
 
@@ -45,12 +88,141 @@ public class HomeActivity extends AppCompatActivity {
         ab.setDisplayShowCustomEnabled(true);
 
 
+        //show and hide category layout when wxpanded and collapsed
 
-        //change banner
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+
+                if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+
+                   categorylayout.setVisibility(View.INVISIBLE);
+
+                    //LinearLayout layout = (LinearLayout)findViewById(R.id.adlyt_id);
+                 //   LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                  //  params.setMargins(30, 20, 30, 0);
+                  //  layout.setLayoutParams(params);
+
+
+                } else if (verticalOffset == 0) {
+
+                   // LinearLayout layout = (LinearLayout)findViewById(R.id.adlyt_id);
+                   // LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                  //  params.setMargins(30, 80, 30, 0);
+                  //  layout.setLayoutParams(params);
+
+                    categorylayout.setVisibility(View.VISIBLE);
+                }
+               else {
+                    categorylayout.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+//show items in grid view
+
+        gridView = (ExpandableHeightGridView) findViewById(R.id.grid);
+        gridView.setExpanded(true);
+
+            images = new ArrayList<>();
+            names = new ArrayList<>();
+            color = new ArrayList<>();
+
+
+            getData();
+
+
+            gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View v,
+                                        int position, long id) {
+
+                    Toast.makeText(HomeActivity.this, String.valueOf(names.get(position)), Toast.LENGTH_SHORT).show();
+
+                }
+            });
+
+
+        }
+
+        private void getData(){
+            //Showing a progress dialog while our app fetches the data from url
+         //   final ProgressDialog loading = ProgressDialog.show(this, "Please wait...","Fetching data...",false,false);
+
+            //Creating a json array request to get the json from our api
+            JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(DATA_URL,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray response) {
+                            //Dismissing the progressdialog on response
+                   //         loading.dismiss();
+
+                            //Displaying our grid
+                            showGrid(response);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+
+                        }
+                    }
+            );
+
+            //Creating a request queue
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            //Adding our request to the queue
+            requestQueue.add(jsonArrayRequest);
+        }
+
+
+        private void showGrid(JSONArray jsonArray){
+            //Looping through all the elements of json array
+            for(int i = 0; i<jsonArray.length(); i++){
+                //Creating a json object of the current index
+                JSONObject obj = null;
+                try {
+                    //getting json object from current index
+                    obj = jsonArray.getJSONObject(i);
+
+                    //getting image url and title from json object
+                    images.add(obj.getString(TAG_IMAGE_URL));
+                    names.add(obj.getString(TAG_NAME));
+                    color.add(obj.getString(TAG_COLOR));
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+            //Creating GridViewAdapter Object
+            GridViewAdapter gridViewAdapter = new GridViewAdapter(this,images,names,color);
+
+            //Adding adapter to gridview
+            gridView.setAdapter(gridViewAdapter);
+        }
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
-
-
-
-}
--- src/main/java/com/webquiver/lelu/LoginActivity.java
+++ src/main/java/com/webquiver/lelu/LoginActivity.java
@@ -1,27 +1,66 @@
 package com.webquiver.lelu;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
+import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText usernameET,passwordET;
+    ImageView loginbtn;
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        usernameET=(EditText)findViewById(R.id.username_id);
-        passwordET=(EditText)findViewById(R.id.password_id);
+
+        //apply font
+        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
+                .setDefaultFontPath("fonts/latoregular.ttf")
+                .setFontAttrId(R.attr.fontPath)
+                .build()
+        );
 
 
+        usernameET = (EditText) findViewById(R.id.username_id);
+        passwordET = (EditText) findViewById(R.id.password_id);
+        loginbtn = (ImageView) findViewById(R.id.loginbtn_id);
+
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //code for the redlinr in edittext
 
         usernameET.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -29,20 +68,20 @@ public class LoginActivity extends AppCompatActivity {
 
                 usernameET.setFocusable(true);
                 usernameET.setCursorVisible(true);
-              //  (usernameET).setCompoundDrawablesWithIntrinsicBounds(R.drawable.txtovr, 0, 0, 0);
-              //  (passwordET).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+                //  (usernameET).setCompoundDrawablesWithIntrinsicBounds(R.drawable.txtovr, 0, 0, 0);
+                //  (passwordET).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
                 usernameET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_selected));
-               passwordET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_shape_rounded));
+                passwordET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_shape_rounded));
                 return false;
 
             }
         });
 
-      passwordET.setOnTouchListener(new View.OnTouchListener() {
+        passwordET.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 passwordET.setFocusable(true);
-                 passwordET.setCursorVisible(true);
+                passwordET.setCursorVisible(true);
                 passwordET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_selected));
                 usernameET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_shape_rounded));
                 return false;
@@ -50,7 +89,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
 
 
         usernameET.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -61,7 +99,6 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
 
 
-
             }
         });
 
@@ -70,13 +107,28 @@ public class LoginActivity extends AppCompatActivity {
                 passwordET.setCursorVisible(false);
                 passwordET.setBackground(ContextCompat.getDrawable(getApplicationContext(), R.drawable.edittext_shape_rounded));
                 return false;
-
-
-
             }
         });
 
 
+
+//check email format correct or not
+        usernameET.addTextChangedListener(new TextWatcher(){
+            public void afterTextChanged(Editable s) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after){}
+            public void onTextChanged(CharSequence s, int start, int before, int count){
+
+                if (isEmailValid(usernameET.getText().toString()))
+                {
+                    (usernameET).setCompoundDrawablesWithIntrinsicBounds(0, 0, R.mipmap.smalltick, 0);
+
+
+                }
+                else
+                    (usernameET).setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+
+            }
+        });
 
 
 
@@ -88,6 +140,22 @@ public class LoginActivity extends AppCompatActivity {
 
 
     }
+
+
+
+        public static boolean isEmailValid(String email) {
+            String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email);
+            return matcher.matches();
+        }
+
+
+
+
+
+
+
 
 
 
